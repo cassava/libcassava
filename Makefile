be55be2abf96f625b6031c933dc48ddf6855ddab
Ben Morgan <neembi@googlemail.com>
2012-05-18 17:58:32 +0200

    Fixing a fatal bug and adding new debug.h header
    The list_free_all() function in debug.h would stomp over all your
    memory and try to free it, because I forgot a *.
    
    The debug.h header provides some macros to print or run debugging
    code unless NDEBUG is set (which also disables assert statements).

--- Makefile
+++ Makefile
@@ -2,12 +2,18 @@ PREFIX = /usr/local
 LIB_DIR = ${PREFIX}/lib
 INCLUDE_DIR = ${PREFIX}/include
 
-VERSION = 0.1.0
+VERSION = 0.2.0
 
 .PHONY: all install uninstall clean dist
 
 all:
 	make -C src/
+
+dist:
+	install -d dist
+	install -m644 src/*.h dist/
+	install -m644 src/libcassava.so dist/libcassava.so.${VERSION}
+	install -m644 src/libcassava.a dist/
 
 install:
 	install -d ${INCLUDE_DIR}/libcassava
@@ -17,13 +23,14 @@ install:
 	ldconfig ${LIB_DIR}
 
 uninstall:
-	rm -r ${INCLUDE_DIR}/libcassava
-	rm ${LIB_DIR}/libcassava.so.${VERSION}
-	rm ${LIB_DIR}/libcassava.a
+	test -d ${INCLUDE_DIR}/libcassava && rm -r ${INCLUDE_DIR}/libcassava
+	test -f ${LIB_DIR}/libcassava.so.${VERSION} && rm ${LIB_DIR}/libcassava.so.${VERSION}
+	test -f ${LIB_DIR}/libcassava.a && rm ${LIB_DIR}/libcassava.a
 	ldconfig ${LIB_DIR}
 
 clean:
 	make -C src/ clean
-	rm libcassava-${VERSION}.tar.gz
+	test -d dist && rm -rf dist
+	test -w libcassava-${VERSION}.tar.gz && rm libcassava-${VERSION}.tar.gz
 
 # vim: set noet:
--- src/list.c
+++ src/list.c
@@ -18,6 +18,7 @@
 
 #include "list.h"
 
+#include <assert.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
@@ -85,6 +86,8 @@ void list_push(struct list_node **head, void *data)
 {
     struct list_node *node = list_node();
     node->data = data;
+    assert(head != NULL);
+
     node->next = *head;
     *head = node;
 }
@@ -94,6 +97,7 @@ void *list_pop(struct list_node **head)
     struct list_node *next;
     void *data;
 
+    assert(head != NULL);
     if (list_empty(*head))
         return NULL;
 
@@ -108,6 +112,7 @@ void list_insert(struct list_node **head, struct list_node *node)
 {
     struct list_node *tail;
 
+    assert(head != NULL);
     if (list_empty(*head)) {
         *head = node;
         return;
@@ -127,6 +132,7 @@ struct list_node *list_remove(struct list_node **head)
     struct list_node *top;
     struct list_node *next;
 
+    assert(head != NULL);
     if (list_empty(*head))
         return NULL;
 
@@ -139,6 +145,7 @@ struct list_node *list_remove(struct list_node **head)
 
 void list_free_nodes(struct list_node **head)
 {
+    assert(head != NULL);
     while (*head != NULL) {
         struct list_node *temp = (*head)->next;
         free(*head);
@@ -149,7 +156,8 @@ void list_free_nodes(struct list_node **head)
 
 void list_free_all(struct list_node **head)
 {
-    while (head != NULL) {
+    assert(head != NULL);
+    while (*head != NULL) {
         struct list_node *temp;
         if ((*head)->data != NULL)
             free((*head)->data);
